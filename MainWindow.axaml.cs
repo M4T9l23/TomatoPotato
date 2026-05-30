640431ad5467754f48f2661678507adb6232b7e0
M4T9l23 <mirajira8@gmail.com>
2026-05-30 22:25:46 +0200

    UI first try

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,26 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace AvaloniaLinuxTest;
-
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnHelloClick(object? sender, RoutedEventArgs e)
+    {
+        StatusText.Text = "Hello there! 👋";
+    }
+
+    private void OnChangeTextClick(object? sender, RoutedEventArgs e)
+    {
+        StatusText.Text = "The text has been changed.";
+    }
+
+    private void OnCloseClick(object? sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
